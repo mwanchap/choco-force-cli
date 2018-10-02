9ef7289cdf246d4021441fe02467f738ea67f5cf
Matt Wanchap <matt.wanchap@cpal.com.au>
2018-10-03 07:39:33 +1000

    Updated to new release v0.25.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ForceCLI/force/releases/download/v0.24.3/force_windows_386.exe'
-$url64      = 'https://github.com/ForceCLI/force/releases/download/v0.24.3/force_windows_amd64.exe'
+$url        = 'https://github.com/ForceCLI/force/releases/download/v0.25.0/force_windows_386.exe'
+$url64      = 'https://github.com/ForceCLI/force/releases/download/v0.25.0/force_windows_amd64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   FileFullPath  = "$toolsDir\force.exe"
   url           = $url
   url64bit      = $url64
-  checksum      = '940FB894AE42833B22EC41C624751126D763719DCFBC4F3F0AD0F0D1A2228D33'
+  checksum      = '153358408A0CC80D904C23760616F46CCEF29A65798BB645C149420C3AD796B1'
   checksumType  = 'sha256'
-  checksum64    = 'F1321D136BAB13C066CED14A6901359AB93FA68DFBDAC494D33E50328029EE50'
+  checksum64    = '0AC2C961F33E668FB991788DA7811A2EA9013D10DFA63E6D3EB8A4EC8A667706'
   checksumType64= 'sha256'
 
 }
